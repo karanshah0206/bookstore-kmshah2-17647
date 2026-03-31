3ed8ed483e605a61456a01ccf9b6fe5372a4003c
Karan Shah <karanshah0206@outlook.com>
2026-03-30 21:07:10 -0400

    Implementations to create book response types.

--- bookstore_web_app/src/dto/book.rs
+++ bookstore_web_app/src/dto/book.rs
@@ -1,5 +1,5 @@
 // Bookstore Web App Service
-//! Data Transfer Objects for Book Entities
+//! Data Transfer Objects for Book Entities.
 // Author: Karan Manoj Shah <kmshah2@cs.cmu.edu>
 
 use serde::{Deserialize, Serialize};
@@ -7,29 +7,59 @@ use serde::{Deserialize, Serialize};
 /// Expected request schema for creating a new book entity.
 #[derive(Deserialize)]
 pub struct CreateBookRequest {
-  isbn: String,
-  title: String,
-  author: String,
-  description: String,
-  genre: String,
-  price: f64,
-  quantity: usize,
+  pub isbn: String,
+  pub title: String,
+  pub author: String,
+  pub description: String,
+  pub genre: String,
+  pub price: f64,
+  pub quantity: usize,
 }
 
 /// Response schema for successfully creating a new book entity.
 #[derive(Serialize)]
 pub struct CreatedBookResponse {
-  isbn: String,
-  title: String,
-  author: String,
-  description: String,
-  genre: String,
-  price: f64,
-  quantity: usize,
+  pub isbn: String,
+  pub title: String,
+  pub author: String,
+  pub description: String,
+  pub genre: String,
+  pub price: f64,
+  pub quantity: usize,
+}
+
+impl CreatedBookResponse {
+  /// Create a book creation success response.
+  pub fn new(
+    isbn: String,
+    title: String,
+    author: String,
+    description: String,
+    genre: String,
+    price: f64,
+    quantity: usize,
+  ) -> Self {
+    CreatedBookResponse {
+      isbn,
+      title,
+      author,
+      description,
+      genre,
+      price,
+      quantity,
+    }
+  }
 }
 
 /// Response schema for failure in creating a new book entity.
 #[derive(Serialize)]
 pub struct FailedBookCreationResponse {
-  message: String,
+  pub message: String,
+}
+
+impl FailedBookCreationResponse {
+  /// Create a book creation failure response.
+  pub fn new(message: String) -> Self {
+    FailedBookCreationResponse { message }
+  }
 }
