a1f23cc92fa72ea55b43b643227726a3b6ea9c91
Karan Shah <karanshah0206@outlook.com>
2026-04-18 02:52:26 -0400

    Update deployment scripts

--- deployment/init_books_db.sql
+++ deployment/init_books_db.sql
@@ -3,14 +3,4 @@
 
 CREATE DATABASE IF NOT EXISTS bookstore;
 
-CREATE TABLE `books` (
-  `isbn` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `genre` varchar(255) NOT NULL,
-  `price` double NOT NULL,
-  `quantity` bigint unsigned NOT NULL,
-  `summary` longtext,
-  PRIMARY KEY (`isbn`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `books` (`isbn` varchar(255) NOT NULL, `title` varchar(255) NOT NULL, `author` varchar(255) NOT NULL, `description` varchar(255) NOT NULL, `genre` varchar(255) NOT NULL, `price` double NOT NULL, `quantity` bigint unsigned NOT NULL, `summary` longtext, PRIMARY KEY (`isbn`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
