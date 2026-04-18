b56240a1c98268b696968358a853eba1c74da42e
Karan Shah <karanshah0206@outlook.com>
2026-04-18 03:22:52 -0400

    Script to automatically init DB on CloudFormation deployment

--- deployment/init_books_db.sql
+++ deployment/init_books_db.sql
@@ -3,4 +3,4 @@
 
 CREATE DATABASE IF NOT EXISTS bookstore;
 USE bookstore;
-CREATE TABLE `books` (`isbn` varchar(255) NOT NULL, `title` varchar(255) NOT NULL, `author` varchar(255) NOT NULL, `description` varchar(255) NOT NULL, `genre` varchar(255) NOT NULL, `price` double NOT NULL, `quantity` bigint unsigned NOT NULL, `summary` longtext, PRIMARY KEY (`isbn`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `books` (`isbn` varchar(255) NOT NULL, `title` varchar(255) NOT NULL, `author` varchar(255) NOT NULL, `description` varchar(255) NOT NULL, `genre` varchar(255) NOT NULL, `price` double NOT NULL, `quantity` bigint unsigned NOT NULL, `summary` longtext, PRIMARY KEY (`isbn`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
